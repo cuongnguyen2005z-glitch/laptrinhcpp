9a1960561f196eec06b9d0f34ed3402017b34573
NguyenVanCuong <cuongnguyen2005z@gmail.com>
2026-08-06 12:26:35 +0700

    Add baitap2: nhap va in thong tin

---
+++ baitap2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string Hovaten, lophoc;
+    cout << "Nhap ho va ten: ";
+    getline(cin, Hovaten);
+    cout << "Nhap lop hoc: ";
+    getline(cin, lophoc);
+    cout << "Xin chao, " << Hovaten << ", " << lophoc <<"!" << endl;
+}
