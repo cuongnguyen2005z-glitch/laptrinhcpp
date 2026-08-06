c0a650057a03a025f5b418e4ed41c5f410f693e4
NguyenVanCuong <cuongnguyen2005z@gmail.com>
2026-08-06 12:40:07 +0700

    Add baitap1: in thong tin ca nhan

--- baitap1.cpp
+++ baitap1.cpp
@@ -1,13 +1,21 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Khai báo thư viện iostream để sử dụng các hàm nhập xuất
+using namespace std; // Sử dụng không gian tên std để tránh phải viết std:: trước các hàm và đối tượng chuẩn
 
 int main() {
     cout << "------------------" << endl;
+    // Xuất ra màn hình dấu gạch ngang để phân cách thông tin
     cout << "Ho va ten: Nguyen Van Cuong" << endl;
+    // Xuất ra màn hình họ và tên của học viên
     cout << "Lop: C++ K01" << endl;
+    // Xuất ra màn hình lớp học của học viên
     cout << "Ngay sinh: 13/10/2005" << endl;
+    // Xuất ra màn hình ngày sinh của học viên
     cout << "So thich: Choi the thao, doc sach, hoc hoi nhung dieu moi" << endl;
+    // Xuất ra màn hình sở thích của học viên
     cout << "Muc tieu: Thanh thao C++ de co the thuc hien nhung du an dau tay" << endl;
+    // Xuất ra màn hình mục tiêu của học viên
     cout << "------------------" << endl;
+    // Xuất ra màn hình dấu gạch ngang để phân cách thông tin
     return 0;
+    // Kết thúc chương trình và trả về giá trị 0 để báo hiệu rằng chương trình đã kết thúc thành công
 }
