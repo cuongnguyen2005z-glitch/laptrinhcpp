423a0617e741a9dc44a554af8a6dff625b736972
NguyenVanCuong <cuongnguyen2005z@gmail.com>
2026-08-06 12:22:59 +0700

    toilambaitap

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Moi ban nhap ten: ";
+    cin >> name;
+    cout << "Xin chao, " << name << "!" << endl;
+    return 0;
+}
