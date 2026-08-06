8df777f34fb2fad1080a8267d33335f00a7e6df5
NguyenVanCuong <cuongnguyen2005z@gmail.com>
2026-08-06 12:40:55 +0700

    Add baitap2: nhap va in thong tin

--- baitap2.cpp
+++ baitap2.cpp
@@ -1,11 +1,12 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Khai báo thư viện iostream để sử dụng các hàm nhập xuất
+using namespace std; // Sử dụng không gian tên std để tránh phải viết std:: trước các hàm và đối tượng chuẩn
 
 int main() {
-    string Hovaten, lophoc;
-    cout << "Nhap ho va ten: ";
-    getline(cin, Hovaten);
-    cout << "Nhap lop hoc: ";
-    getline(cin, lophoc);
-    cout << "Xin chao, " << Hovaten << ", " << lophoc <<"!" << endl;
+    string Hovaten, lophoc; //Khai báo biến 
+    cout << "Nhap ho va ten: "; // xuât ra màn hình yêu cầu nhập họ và tên
+    getline(cin, Hovaten); // Nhập họ và tên từ bàn phím
+    cout << "Nhap lop hoc: "; // Xuất ra màn hình yêu cầu nhập lớp học
+    getline(cin, lophoc); // Nhập lớp học từ bàn phím
+    cout << "Xin chao, " << Hovaten << ", " << lophoc <<"!" << endl; 
+    // Xuất ra màn hình lời chào với họ và tên, lớp học
 }
