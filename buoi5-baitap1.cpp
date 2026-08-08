3d6104ace25170d920a03ccb08ec597f5cc2ba18
NguyenVanCuong <cuongnguyen2005z@gmail.com>
2026-08-08 12:44:02 +0700

    buoi5-baitap1

---
+++ buoi5-baitap1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1, b = 2, c = 4;
+    int tong = a + b + c;
+    double diemTB = (a + b + c) / 3.0;
+    cout << "Tong cua 3 so la: " << tong << endl;
+    cout << "Diem trung binh la: " << diemTB << endl;
+    return 0;
+}
